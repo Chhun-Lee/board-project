f4ff025cd6708efafa1a3bb8ccc8c70639f80d50
Chhun-Lee <ckzlsn87@gmail.com>
2025-08-19 12:51:40 +0900

    db 추가 기능

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -1,5 +1,8 @@
 package com.study.board.controller;
 
+import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +11,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class BoardController {
 
+    @Autowired
+    private BoardService boardService;
+
+
     @GetMapping("/board/write") //localhost:8080/board/write
     public String boardWriteForm(){
 
@@ -15,9 +22,9 @@ public class BoardController {
     }
 
     @PostMapping("/board/writepro")
-    public String boardWritePro(String title, String content){
+    public String boardWritePro(Board board){
 
-
+        boardService.write(board);
 
         return "";
     }
